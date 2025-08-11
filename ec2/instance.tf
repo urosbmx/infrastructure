5d288b1bfb17ba155770a3a5a5ef5643edc9e69a
Uros <urosbmx@live.com>
2025-08-11 16:43:07 +0200

    add varibles

--- ec2/instance.tf
+++ ec2/instance.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-  region     = "us-east-1"
+  region     = var.region
 }
 
 resource "aws_instance" "uros" {
-  ami           = "ami-0ca5a2f40c2601df6"
-  instance_type = "t2.micro"
-  key_name   = "test_key_uros"
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name   = var.key_name
   vpc_security_group_ids = ["sg-0cdf2fe27caf96de3"]
 
   tags = {
---
+++ ec2/variable.tf
@@ -0,0 +1,21 @@
+variable "instance_type" {
+    type    = string
+    default = "t2.micro"
+}
+
+variable "ami_id" {
+    type    = string
+    default = "ami-0ca5a2f40c2601df6"
+}
+
+variable "region" {
+    type    = string
+    default = "us-east-1"
+  
+}
+
+variable "key_name" {
+    type    = string
+    default = "test_key_uros"
+}
+
